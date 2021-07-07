9560191e939a1496b8598e346a232859ec186532
Melvin Suter <melvin@suter.dev>
2021-07-07 21:41:11 +0200

    q

--- lib/config.php
+++ lib/config.php
@@ -1,5 +1,5 @@
 <?php
 
-define('CONFIG_BASE_URL','http://'.$_SERVER['SERVER_NAME'].'/blog/public');
+define('CONFIG_BASE_URL','http://'.$_SERVER['SERVER_NAME'].'');
 define('CONFIG_PAGE_TITLE','Some Blog');
 define('CONFIG_PAGE_DESCRIPTION','With some generic description');
