7ba83a827d27ef6665a4b98b9720c2fac8480f3a
Melvin Suter <melvin@suter.dev>
2021-07-15 18:59:25 +0200

    +

--- lib/helpers/common.php
+++ lib/helpers/common.php
@@ -8,8 +8,11 @@ class Common {
     public static function url($url){
         if(preg_match("/http(s*):\/\/.*/",$url))
             return $url;
-            
-        return CONFIG_BASE_URL.$url;
+        
+        if(substr(CONFIG_BASE_URL,-1) == "/")
+            return CONFIG_BASE_URL.$url;
+        else
+            return CONFIG_BASE_URL."/".$url;
     }
 
     public static function getEnv($key, $default = ""){
--- lib/templates/home.php
+++ lib/templates/home.php
@@ -1,4 +1,25 @@
-<?php
-foreach(Post::getNewestPosts() as $post){
-    echo "<div><a href=\"".Common::url($post['slug'])."\">".$post['title']."</a></div>";
-}?>
+<html>
+    <head>
+        <title><?=$entry['title'];?></title>
+        <?php include(__DIR__.'/sub/head.php');?>
+    </head>
+    <body>
+        <div class="container">
+            <?php include(__DIR__.'/sub/navbar.php');?>
+            
+            
+            <div class="card bg-light">
+                <div class="card-body">
+                    <?=$entry['body'];?>
+
+                    <?php
+                    foreach(Post::getNewestPosts() as $post){
+                        echo "<div><a href=\"".Common::url($post['slug'])."\">".$post['title']."</a></div>";
+                    }?>
+                </div>
+            </div>  
+
+            <?php include(__DIR__.'/sub/foot.php');?>
+        </div>
+    </body>
+</html>
